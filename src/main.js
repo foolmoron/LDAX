43d8dcc07163cf07b94bfd1a0cecb3a2bff5a0a2
Momin Khan <foolmoron@gmail.com>
2017-12-03 16:25:09 -0500

    Game start + skeleton

--- src/main.js
+++ src/main.js
@@ -1,8 +1,29 @@
 // elements
 const inpCoinName = $('#inp-coin-name');
+const lblCurrentTicker = $('#lbl-current-ticker');
+const lblCurrentCoin = $('#lbl-current-coin');
 const lblTicker = $('#lbl-ticker');
 const lblOriginalValidation = $('#lbl-original-validation');
 const btnCreateCoin = $('#btn-create-coin');
+const pnlCreate = $('#pnl-create');
+const pnlCandlestick = $('#pnl-candlestick');
+const pnlAssets = $('#pnl-assets');
+const pnlActions = $('#pnl-actions');
+
+// data
+let data = {};
+function load() {
+    try { data = JSON.parse(localStorage.getItem('data1')); } catch(e) { }
+    data = Object.assign({
+        name: '',
+        ticker: '',
+        started: false,
+    }, data);
+}
+function save() {
+    localStorage.setItem('data1', JSON.stringify(data));
+    load();
+}
 
 // misc
 const vowels = ['A','E','I','O','U'];
@@ -17,7 +38,9 @@ function tickerSymbolFromCoinName(coinName) {
         const words = nameClean.split(' ').filter(w => w.length > 0);
         const letters = [null, null, null];
         if (words.length >= 3) {
-            letters = [words[0][0], words[1][0], words[2][0]];
+            letters[0] = words[0][0];
+            letters[1] = words[1][0];
+            letters[2] = words[2][0];
         } else if (words.length == 2) {
             letters[0] = words[0][0];
             letters[2] = words[1][0];
@@ -26,6 +49,7 @@ function tickerSymbolFromCoinName(coinName) {
         }
 
         // any null letters mean there's 2 words of 2 letters each, or 1 word of 4+ letters
+        const coinCashHack = (words[1] == 'CASH' || words[1] == 'COIN') && (nameNoSpaces.match(/C/g) || []).length > 1;
         for (let l = 0; l < letters.length; l++) {
             if (letters[l] != null) {
                 continue;
@@ -38,7 +62,7 @@ function tickerSymbolFromCoinName(coinName) {
                 if (letter == prevLetter && bestLetter != prevLetter) {
                     continue;
                 }
-                if (letter != nextLetter && bestLetter == nextLetter && nextLetter == 'C') {
+                if (letter != nextLetter && bestLetter == nextLetter && coinCashHack) {
                     continue;
                 }
                 if (vowels.indexOf(letter) >= 0 && vowels.indexOf(bestLetter) < 0) {
@@ -64,5 +88,30 @@ function validateCoinName() {
 
 // main
 function createCoin(coinName) {
+    pnlCreate.classList.add('hidden');
+    // reset data
+    data = {};
+    save();
+    // start new game
+    data.name = inpCoinName.value;
+    data.ticker = tickerSymbolFromCoinName(inpCoinName.value);
+    data.started = true;
+    save();
+    startGame();
+}
 
+function startGame() {
+    lblCurrentTicker.textContent = data.ticker;
+    lblCurrentCoin.textContent = `(${data.name})`;
+    pnlCandlestick.classList.remove('hidden');
+    pnlAssets.classList.remove('hidden');
+    pnlActions.classList.remove('hidden');
+}
+
+// start
+load();
+if (data.started) {
+    startGame();
+} else {
+    // TODO: tutorial?? something
 }
