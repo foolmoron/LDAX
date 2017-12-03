7aa93e8dc7de65725ce60740bfdd9ccd16cb8f24
Momin Khan <foolmoron@gmail.com>
2017-12-03 12:56:22 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # LDAX
 LDAX: Ludum Dare Asset Exchange, a cryptocurrency web game for LD40
+
---
+++ src/main.js
@@ -0,0 +1,4 @@
+const list = [1, 2, 3];
+const [a, b] = list;
+document.write(a); // "a"
+document.write(b); // "b"
