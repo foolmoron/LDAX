e563fd0aa6be2e6f2daa6faea3eef7b7474c3344
Momin Khan <foolmoron@gmail.com>
2017-12-03 21:09:49 -0500

    Game loop done

--- src/lib.js
+++ src/lib.js
@@ -1,2 +1,3 @@
 window.$ = (arg) => document.querySelector(arg);
 window.$$ = (arg) => document.querySelectorAll(arg);
+window.lerp = (a, b, t) => a*(1-t) + b*t;
--- src/main.js
+++ src/main.js
@@ -1,15 +1,20 @@
 // elements
 const inpCoinName = $('#inp-coin-name');
-const lblCurrentTicker = $('#lbl-current-ticker');
 const lblCurrentCoin = $('#lbl-current-coin');
-const lblTicker = $('#lbl-ticker');
+const lblTickers = $$('.lbl-ticker');
 const lblOriginalValidation = $('#lbl-original-validation');
+const lblBestRealized = $('#lbl-best-realized');
+const lblBestBoth = $('#lbl-best-both');
+const lblWorstBoth = $('#lbl-worst-both');
+const lblCurrentPrice = $('#lbl-current-price');
+const lblMarketCap = $('#lbl-market-cap');
+const lblCoin = $('#lbl-coin');
+const lblCash = $('#lbl-cash');
 const btnCreateCoin = $('#btn-create-coin');
 const pnlCreate = $('#pnl-create');
 const pnlWorth = $('#pnl-worth');
 const pnlCandlestick = $('#pnl-candlestick');
 const pnlAssets = $('#pnl-assets');
-const pnlActions = $('#pnl-actions');
 const chrWorth = $('#chr-worth');
 const gRealized = $('#g-realized');
 const gUnrealized = $('#g-unrealized');
@@ -19,21 +24,32 @@ const gMarket = $('#g-market');
 
 // data
 let data = {};
+let dataExtra = {
+    bestRealized: 0,
+    bestBoth: 0,
+    worstBoth: Infinity,
+};
 function load() {
     try { data = JSON.parse(localStorage.getItem('data1')); } catch(e) { }
     data = Object.assign({
         name: '',
         ticker: '',
         started: false,
-        cash: 1000,
-        coin: 100,
         tick: 0,
-
+        coins: [1],
+        cashs: [10],
+        prices: [1],
+        actions: [],
+        priceVelocity: 0,
     }, data);
 }
 function save() {
     localStorage.setItem('data1', JSON.stringify(data));
     load();
+}
+
+function getDifficulty() {
+    return Math.max(0, Math.floor(Math.log10(data.prices[data.prices.length - 1])));
 }
 
 // misc
@@ -94,10 +110,38 @@ function tickerSymbolFromCoinName(coinName) {
 const existingCoinNames = ['','bit','ethereum','bitcash','bcash','ripple','dash','lite','bitgold','iota','cardano','monero','ethereumclassic','nem','neo','eos','stellarlumens','bitconnect','omisego','qtum','lisk','zcash','tether','hshare','waves','stratis','populous','ardor','nxt','ark','augur','mona','byte','bitshares','decred','vert','komodo','steem','pivx','salt','golem','sia','status','powerledger','doge','tenx','maidsafe','walton','binance','digixdao','veritaseum','minex'];
 function validateCoinName() {
     const ticker = tickerSymbolFromCoinName(inpCoinName.value);
-    lblTicker.textContent = ticker;
+    updateTickers(ticker);
     const unoriginal = inpCoinName.value && existingCoinNames.indexOf(inpCoinName.value.toLowerCase().replace(/([^\w\s]| |coin)/gi, '')) >= 0;
     lblOriginalValidation.classList.toggle('hidden', !unoriginal);
     btnCreateCoin.classList.toggle('disabled', ticker.length == 0 || unoriginal);
+}
+
+function updateTickers(tickerText) {
+    for (ticker of lblTickers) {
+        ticker.textContent = tickerText;
+    }
+}
+
+function updateWorstBoth() {
+    dataExtra.worstBoth = Infinity;
+    for (let i = 0; i < 100 && i < data.prices.length; i++) {
+        const both = data.cashs[data.cashs.length - 1] + (data.prices[data.prices.length - 1] * data.coins[data.coins.length - 1]);
+        dataExtra.worstBoth = Math.min(both, dataExtra.worstBoth);
+    }
+}
+
+function setMoneyLabel(label, amount) {
+    label.textContent = amount.toFixed(2);
+}
+
+function updateMoneyLabels() {
+    setMoneyLabel(lblBestRealized, dataExtra.bestRealized);
+    setMoneyLabel(lblBestBoth, dataExtra.bestBoth);
+    setMoneyLabel(lblWorstBoth, dataExtra.worstBoth);
+    setMoneyLabel(lblCurrentPrice, data.prices[data.prices.length - 1]);
+    setMoneyLabel(lblMarketCap, data.prices[data.prices.length - 1] + 21000);
+    setMoneyLabel(lblCoin, data.coins[data.coins.length - 1]);
+    setMoneyLabel(lblCash, data.cashs[data.cashs.length - 1]);
 }
 
 // charts
@@ -114,7 +158,7 @@ function updateChartSize(group, max) {
     var newMaxY = Math.max(max.y, chart.dataset.maxY || 0);
     chart.dataset.maxX = newMaxX;
     chart.dataset.maxY = newMaxY;
-    chart.setAttribute('viewBox', `0 0 ${newMaxX} ${newMaxY}`);
+    chart.setAttribute('viewBox', `0 0 ${newMaxX} ${newMaxY*1.1}`);
     chart.style.width = newMaxX;
 }
 function scrollToRightNextFrame(group, force) {
@@ -138,7 +182,7 @@ function renderLineChart(group, points) {
         line.setAttribute('x1', (p - 1) * TICK_WIDTH);
         line.setAttribute('y1', prevPoint);
         line.setAttribute('x2', p * TICK_WIDTH);
-        line.setAttribute('y2', point);
+        line.setAttribute('y2', point + (point * Math.random()*0.01));
         group.appendChild(line);
         prevPoint = point;
     }
@@ -149,10 +193,10 @@ function addPointToLineChart(group, points, point) {
     const prevPoint = points[points.length - 1] || 0;
     points.push(point);
     const line = document.createElementNS("http://www.w3.org/2000/svg", 'line');
-    line.setAttribute('x1', (points.length - 1) * TICK_WIDTH);
-    line.setAttribute('y1', prevPoint.y);
+    line.setAttribute('x1', (points.length - 2) * TICK_WIDTH);
+    line.setAttribute('y1', prevPoint);
     line.setAttribute('x2', (points.length - 1) * TICK_WIDTH);
-    line.setAttribute('y2', point.y);
+    line.setAttribute('y2', point + (point * Math.random()*0.01));
     group.appendChild(line);
     updateChartSize(group, getMax(points));
 }
@@ -166,7 +210,7 @@ function renderCandlestick(group, points) {
     let x = 0;
     for (point of points) {
         const bottom = Math.min(prevPoint, point);
-        const height = Math.abs(point - prevPoint);
+        const height = Math.max(0.01, Math.abs(point - prevPoint));
         const rect = document.createElementNS("http://www.w3.org/2000/svg", 'rect');
         rect.setAttribute('x', x + TICK_WIDTH * (CANDLESTICK_GAP / 2));
         rect.setAttribute('y', bottom);
@@ -176,9 +220,9 @@ function renderCandlestick(group, points) {
         group.appendChild(rect);
         const line = document.createElementNS("http://www.w3.org/2000/svg", 'line');
         line.setAttribute('x1', x + TICK_WIDTH/2);
-        line.setAttribute('y1', bottom - (Math.max(height, 20) * Math.random() * Math.random() * 0.7));
+        line.setAttribute('y1', bottom - (Math.min(height, 20) * Math.random() * Math.random() * 0.7));
         line.setAttribute('x2', x + TICK_WIDTH/2);
-        line.setAttribute('y2', bottom + height + (Math.max(height, 20) * Math.random() * Math.random() * 0.7));
+        line.setAttribute('y2', bottom + height + (Math.min(height, 20) * Math.random() * Math.random() * 0.7));
         line.classList.add(point >= prevPoint ? 'up' : 'down');
         group.appendChild(line);
         x += TICK_WIDTH;
@@ -191,7 +235,7 @@ function addPointToCandlestick(group, points, point) {
     const prevPoint = points[points.length - 1] || 0;
     points.push(point);
     const bottom = Math.min(prevPoint, point);
-    const height = Math.abs(point - prevPoint);
+    const height = Math.max(0.01, Math.abs(point - prevPoint));
     const rect = document.createElementNS("http://www.w3.org/2000/svg", 'rect');
     rect.setAttribute('x', (points.length - 1) * TICK_WIDTH + TICK_WIDTH * (CANDLESTICK_GAP / 2));
     rect.setAttribute('y', bottom);
@@ -201,9 +245,9 @@ function addPointToCandlestick(group, points, point) {
     group.appendChild(rect);
     const line = document.createElementNS("http://www.w3.org/2000/svg", 'line');
     line.setAttribute('x1', (points.length - 0.5) * TICK_WIDTH);
-    line.setAttribute('y1', bottom - (Math.max(height, 20) * Math.random() * Math.random() * 0.7));
+    line.setAttribute('y1', bottom - (Math.min(height, 20) * Math.random() * Math.random() * 0.7));
     line.setAttribute('x2', (points.length - 0.5) * TICK_WIDTH);
-    line.setAttribute('y2', bottom + height + (Math.max(height, 20) * Math.random() * Math.random() * 0.7));
+    line.setAttribute('y2', bottom + height + (Math.min(height, 20) * Math.random() * Math.random() * 0.7));
     line.classList.add(point >= prevPoint ? 'up' : 'down');
     group.appendChild(line);
     updateChartSize(group, getMax(points));
@@ -224,12 +268,107 @@ function createCoin(coinName) {
 }
 
 function startGame() {
-    lblCurrentTicker.textContent = data.ticker;
+    updateTickers(data.ticker);
     lblCurrentCoin.textContent = `(${data.name})`;
     pnlWorth.classList.remove('hidden');
     pnlCandlestick.classList.remove('hidden');
     pnlAssets.classList.remove('hidden');
-    pnlActions.classList.remove('hidden');
+
+    // calc worths
+    dataExtra.realized = new Array(data.prices.length);
+    dataExtra.unrealized = new Array(data.prices.length);
+    dataExtra.both = new Array(data.prices.length);
+    for (let i = 0; i < data.prices.length; i++) {
+        dataExtra.realized[i] = data.cashs[i];
+        dataExtra.unrealized[i] = data.prices[i] * data.coins[i];
+        dataExtra.both[i] = dataExtra.realized[i] + dataExtra.unrealized[i];
+
+        dataExtra.bestRealized = Math.max(dataExtra.realized[i], dataExtra.bestRealized);
+        dataExtra.bestBoth = Math.max(dataExtra.realized[i] + dataExtra.unrealized[i], dataExtra.bestBoth);
+    }
+    updateWorstBoth();
+    // render worths
+    renderLineChart(gRealized, dataExtra.realized);
+    renderLineChart(gUnrealized, dataExtra.unrealized);
+    renderLineChart(gBoth, dataExtra.both);
+    // render prices
+    renderCandlestick(gMarket, data.prices);
+    // labels
+    updateMoneyLabels();
+
+    // tick
+    tick();
+}
+
+let tickTimeout = 0;
+function tick(coinChange) {
+    const price = data.prices[data.prices.length - 1];
+    const coin = data.coins[data.coins.length - 1];
+    const cash = data.cashs[data.cashs.length - 1];
+    
+    // coins
+    if (coinChange != null) {
+        if (coinChange > 0) {
+            const coinToBuy = Math.min(cash/price, coinChange);
+            if (coinToBuy == 0) {
+                return;
+            }
+            data.priceVelocity += Math.pow(coinToBuy, 1.5);
+            data.coins.push(coin + coinToBuy);
+            data.cashs.push(cash - coinToBuy*price);
+        } else {
+            const coinToSell = Math.min(coin, -coinChange);
+            if (coinToSell == 0) {
+                return;
+            }
+            data.priceVelocity += Math.pow(coinToSell, 2.7);
+            data.coins.push(coin - coinToSell);
+            data.cashs.push(cash + coinToSell*price);
+        }
+    } else {
+        data.coins.push(coin);
+        data.cashs.push(cash);
+    }
+
+    const newCoin = data.coins[data.coins.length - 1];
+    const newCash = data.cashs[data.cashs.length - 1]
+
+    // add to price based on velocity with random shift
+    const difficulty = getDifficulty();
+    const coinValue = coin * price;
+    const coinPerc = coinValue / Math.max(0.00001, coinValue + cash);
+    const priceDelta = (data.priceVelocity * lerp(0.25, 2.5, Math.random() * Math.random())) * (Math.random() < (0.15*difficulty) ? -1 : 1);
+    const priceDelta2 = Math.sign(data.priceVelocity) * (coinPerc - 0.5) * (price / (difficulty + 1)) * (lerp(0.1, 1, Math.random() * Math.random()) / (difficulty + 1));
+    const newPrice = Math.max(0.001, price + priceDelta + priceDelta2);
+
+    // decay and flip
+    data.priceVelocity = data.priceVelocity * 0.7;
+    if ((data.priceVelocity > 0 && Math.random() < (coinPerc + 0.1)) || (data.priceVelocity < 0 && Math.random() > (coinPerc - 0.1))) {
+        data.priceVelocity = -1 * Math.sign(data.priceVelocity) * lerp(0.05, 0.2, Math.random()) * price;
+    }
+
+    // data/charts
+    addPointToCandlestick(gMarket, data.prices, newPrice);
+    const realized = newCash;
+    addPointToLineChart(gRealized, dataExtra.realized, realized);
+    const unrealized = newPrice * newCoin;
+    addPointToLineChart(gUnrealized, dataExtra.unrealized, unrealized);
+    const both = realized + unrealized;
+    addPointToLineChart(gBoth, dataExtra.both, both);
+
+    // update stats
+    dataExtra.bestRealized = Math.max(realized, dataExtra.bestRealized);
+    dataExtra.bestBoth = Math.max(both, dataExtra.bestBoth);
+    updateWorstBoth();
+    updateMoneyLabels();
+
+    // next tick
+    const nextTickTime = 1000 / Math.sqrt(difficulty + 1);
+    clearTimeout(tickTimeout);
+    tickTimeout = setTimeout(tick, nextTickTime);
+
+    // save each tick
+    save();
 }
 
 // start
@@ -237,10 +376,5 @@ load();
 if (data.started) {
     startGame();
 } else {
-    // TODO: tutorial?? something
+    pnlCreate.classList.remove('hidden');
 }
-
-renderLineChart(gRealized, Array.from(Array(1000), (d, i) => Math.sqrt(i*100)*Math.random() * 1000));
-renderLineChart(gUnrealized, Array.from(Array(1000), (d, i) => Math.sqrt(i*100)*Math.random() * 1000));
-renderLineChart(gBoth, Array.from(Array(1000), (d, i) => Math.sqrt(i*100)*Math.random() * 1000));
-renderCandlestick(gMarket, Array.from(Array(1000), (d, i) => Math.sqrt(i*100)*Math.random() * 1000));
